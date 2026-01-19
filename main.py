1b271da4fdfd86a871c8a92e9f0e16069aa509f1
jhonXtung <jhon.pizarra@urios.edu.ph>
2026-01-19 15:50:38 +0800

    Added 67

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello NIGGER") 
+def main():
+    print("--- Jhon Tung's Place ---")
+    
+
+    year_born = input("What year were you born?: ")
+    age = 2026 - year_born
+	
+    print(f"You will be {age} in 2026!")
+
+if __name__ == "__main__":
+    main()
